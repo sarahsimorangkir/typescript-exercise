da552f4ab69587ce7ac7328ab4ebca8118f541ad
Sarah Simorangkir <sarahsimorangkir34@gmail.com>
2022-04-20 09:14:30 +0700

    add change

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
 import { render, screen } from '@testing-library/react';
-import App from './App';
+import Home from './pages/home';
 
 test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
+  render(<Home/>);
+  const linkElement = screen.getByText(/Giphyeyy/i);
   expect(linkElement).toBeInTheDocument();
 });
